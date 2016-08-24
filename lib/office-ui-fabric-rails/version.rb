e84cc3a9c8491a97035026c3e91ba3d35f1421c8
Craig Plummer <c.plummer2@herts.ac.uk>
2016-08-24 11:06:55 +0100

    Upgrade to office-ui-fabric-core 4.0.0

--- lib/office-ui-fabric-rails/version.rb
+++ lib/office-ui-fabric-rails/version.rb
@@ -1,7 +1,7 @@
 module OfficeUiFabric
   module Rails
-    FABRIC_CORE_VERSION = "3.1.0"
+    FABRIC_CORE_VERSION = "4.0.0"
     FABRIC_JS_VERSION = "3.0.0-beta2"
-    VERSION = "3.1.0.1"
+    VERSION = "4.0.0.0"
   end
 end
